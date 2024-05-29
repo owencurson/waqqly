442d9ffc12b43fab920d1c35f6f39888743dd8aa
owencurson <o.curson.21@unimail.winchester.ac.uk>
2024-05-29 18:29:28 +0100

    change

--- views/scripts/main.js
+++ views/scripts/main.js
@@ -1,3 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
     console.log('DOM fully loaded and parsed');
+    const button = document.querySelector('button');
+    button.addEventListener('click', () => {
+        alert('Button clicked!');
+    });
 });
