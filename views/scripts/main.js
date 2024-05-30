a0b507161cbd9d22dd48e6af8ac6c489d87a1027
owencurson <o.curson.21@unimail.winchester.ac.uk>
2024-05-30 11:03:26 +0100

    main

--- views/scripts/main.js
+++ views/scripts/main.js
@@ -3,7 +3,7 @@ document.getElementById('pet-form').addEventListener('submit', async (event) => 
     const petName = document.getElementById('pet-name').value;
     const petBreed = document.getElementById('pet-breed').value;
 
-    const response = await fetch('https://<your-function-app-name>.azurewebsites.net/api/registerPet', {
+    const response = await fetch('https://waqqlyapp.azurewebsites.net/api/registerPet', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
@@ -19,7 +19,7 @@ document.getElementById('walker-form').addEventListener('submit', async (event) 
     const walkerName = document.getElementById('walker-name').value;
     const walkerLocation = document.getElementById('walker-location').value;
 
-    const response = await fetch('https://<your-function-app-name>.azurewebsites.net/api/registerWalker', {
+    const response = await fetch('https://waqqlyapp.azurewebsites.net/api/registerWalker', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
